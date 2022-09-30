d87ae855151348c08bafcce01fea2be1e3d73524
TheoVauvilliers <theo.vauvilliers10@gmail.com>
2022-09-30 20:53:32 +0200

    Update comments

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
 
     list_of_files = create_list_of_files("public/upload")
 
-    # initialise variables for the loop
     number_rows_inserted = 0
     iteration_number = 1
     timer_start = time()
@@ -27,28 +26,22 @@ if __name__ == "__main__":
         for index, row in data.iterrows():
             rows.append(row.array)
 
-            # execute the bulk operation every BULK_SIZE rows
+            # every BULK_SIZE rows, insert them into the database
             if iteration_number % BULK_SIZE == 0:
-                # execute the bulk operation
                 bulk_execute(collection, rows)
-                # reset the rows list
                 rows = []
-                # reset the counter
                 iteration_number = 1
 
-            # each LOG_ROWS_NUMBER rows, print the number of rows inserted
+            # every LOG_ROWS_NUMBER rows, print the number of rows inserted
             if number_rows_inserted % LOG_ROWS_NUMBER == 0:
                 pprint("--- --- --- ---")
                 pprint(f"Number of rows inserted: {round(number_rows_inserted / 1000)}K")
                 pprint(f"Time elapsed: {round(time() - timer_start, 2)}s")
-
-                # reset the timer
                 timer_start = time()
 
             number_rows_inserted += 1
             iteration_number += 1
     
-    # Execute the last bulk operation if is needed
     if iteration_number != 1:
         bulk_execute(collection, rows)
 
